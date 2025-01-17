da0cbebedabb0f41ad30f04d714aa40ee08cdbfd
Nazaliqbaal <nazaliqbal786@gmail.com>
2025-01-17 11:32:24 +0530

    cange env

--- src/app/createChatCompletion.ts
+++ src/app/createChatCompletion.ts
@@ -5,15 +5,12 @@ import { ChatCompletionMessage } from "./chat-completion-messages.interface";
 export default async function createChatCompletion(
   messages: ChatCompletionMessage[]
 ) {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/openai/chatCompletion`,
-    {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({ messages }),
-    }
-  );
+  const response = await fetch(`${process.env.API_URL}/openai/chatCompletion`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({ messages }),
+  });
   return response.json();
 }
